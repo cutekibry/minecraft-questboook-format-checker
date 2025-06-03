99a1c9513e9ad456c124b9995fb012260fcd8e9f
Tsukimaru Oshawott <cutekibry@gmail.com>
2025-06-03 18:36:42 +0800

    refactor: update configs for more styles

--- main.py
+++ main.py
@@ -1,99 +1,76 @@
 from enum import Enum
 from pydantic import BaseModel
-from typing import Literal, Self, Tuple
+from typing import Self, Tuple
 import re
 import rich
 from rich.table import Table
 import typer
 from pathlib import Path
 
-STYLE_CODES = [
-    "0",
-    "1",
-    "2",
-    "3",
-    "4",
-    "5",
-    "6",
-    "7",
-    "8",
-    "9",
-    "a",
-    "b",
-    "c",
-    "d",
-    "e",
-    "f",
-    "ENDLINE",
-    "k",
-    "l",
-    "m",
-    "n",
-    "o",
-    "r",
-]
+CODE_OBFUSCATED = "k"
+CODE_BOLD = "l"
+CODE_STRIKETHROUGH = "m"
+CODE_UNDERLINE = "n"
+CODE_ITALIC = "o"
+CODE_RESET = "r"
 
-type Color = Literal[
-    "0",
-    "1",
-    "2",
-    "3",
-    "4",
-    "5",
-    "6",
-    "7",
-    "8",
-    "9",
-    "a",
-    "b",
-    "c",
-    "d",
-    "e",
-    "f",
-    "ENDLINE",
-    "k",
-    "l",
-    "m",
-    "n",
-    "o",
-    "r",
-]
+STYLE_CODES = (
+    "0123456789abcdef"
+    + CODE_OBFUSCATED
+    + CODE_BOLD
+    + CODE_STRIKETHROUGH
+    + CODE_UNDERLINE
+    + CODE_ITALIC
+    + CODE_RESET
+)
+
+
+class TextStyle(BaseModel):
+    color: str = "0"
+    is_bold: bool = False
+    is_italic: bool = False
+    is_underline: bool = False
+    is_strikethrough: bool = False
+    is_obfuscated: bool = False
 
 
 class TextEntry(BaseModel):
-    color: Color
+    style: TextStyle
     content: str
+
+    def can_be_ignored(self) -> bool:
+        return self.style.color == "0"
 
 
 class Article(BaseModel):
     texts: list[TextEntry]
 
+    @staticmethod
+    def raise_if_invalid(raw_article: str) -> None:
+        invalid_matches = re.findall(r"%(.)", raw_article)
+        for match in invalid_matches:
+            if match[0] not in ["n", "%"]:
+                rich.print(f'[red]Invalid matches: "%{match[0]}"[/red]')
+                raise ValueError(f'Invalid matches: "%{match[0]}"')
+
+        invalid_matches = re.findall(r"§(.)", raw_article)
+        for match in invalid_matches:
+            if match[0] not in STYLE_CODES:
+                rich.print(f'[red]Invalid matches: "§{match[0]}"[/red]')
+                raise ValueError(f'Invalid matches: "§{match[0]}"')
+
     @classmethod
     def parse_questbook_string(cls, raw_article: str) -> Self:
+        cls.raise_if_invalid(raw_article)
+
         texts: list[TextEntry] = []
 
         text_lines = raw_article.split("%n")
         for line in text_lines:
-            invalid_matches = re.findall(r"%(.)", line)
-            for match in invalid_matches:
-                if match[0] not in ["n", "%"]:
-                    rich.print(
-                        f'[red]Invalid matches: "%{match[0]}" in line "{line}"[/red]'
-                    )
-                    raise ValueError(f'Invalid matches: "%{match[0]}"')
-
-            invalid_matches = re.findall(r"§(.)", line)
-            for match in invalid_matches:
-                if match[0] not in STYLE_CODES:
-                    rich.print(
-                        f'[red]Invalid matches: "§{match[0]}" in line "{line}"[/red]'
-                    )
-                    raise ValueError(f'Invalid matches: "§{match[0]}"')
-
             if texts:
-                texts.append(TextEntry(color="ENDLINE", content="%n"))
+                texts.append(TextEntry(style=TextStyle(color="ENDLINE"), content="%n"))
 
-            current_color: Color = "0"
+            current_color: str = "0"
 
             text_matches = re.findall(r"(§[0-9a-fr])|([^§]+)", line)
             for match in text_matches:
@@ -103,7 +80,11 @@ class Article(BaseModel):
                     else:
                         current_color = match[0][1]
                 else:
-                    texts.append(TextEntry(color=current_color, content=match[1]))
+                    texts.append(
+                        TextEntry(
+                            style=TextStyle(color=current_color), content=match[1]
+                        )
+                    )
 
         return cls(texts=texts)
 
@@ -119,10 +100,10 @@ def format_text(
     text: TextEntry | None, style: str | None = None, *, is_color_all: bool = True
 ) -> str:
     if isinstance(text, TextEntry):
-        if text.color == "ENDLINE":
+        if text.style.color == "ENDLINE":
             return f"[{style}]{text.content}[/{style}]" if style else text.content
 
-        color_as_str: str = f"§{text.color}"
+        color_as_str: str = f"§{text.style.color}"
         content_as_str: str = text.content
 
         if style is not None:
@@ -153,25 +134,25 @@ def get_diff(article_1: Article, article_2: Article) -> Tuple[bool, Table]:
         return article_2.texts[index_2]
 
     while index_1 < len(article_1.texts) and index_2 < len(article_2.texts):
-        if a().color == "0" and b().color == "0":
+        if a().can_be_ignored() and b().can_be_ignored():
             # Both are normal text and accepted.
             table.add_row(format_text(a()), format_text(b()))
             index_1 += 1
             index_2 += 1
 
-        elif a().color == "0" and b().color != "0":
+        elif a().can_be_ignored() and not b().can_be_ignored():
             # en() is normal text, tr() is not.
             table.add_row(format_text(a(), style="yellow", is_color_all=True), "")
             index_1 += 1
 
-        elif a().color != "0" and b().color == "0":
+        elif not a().can_be_ignored() and b().can_be_ignored():
             # en() is not normal text, tr() is normal text.
             table.add_row("", format_text(b(), style="yellow", is_color_all=True))
             index_2 += 1
 
         else:
             # both are not normal text.
-            if a().color == b().color:
+            if a().style == b().style:
                 table.add_row(format_text(a()), format_text(b()))
             else:
                 table.add_row(
